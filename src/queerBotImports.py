8e89e312ea28c012b2d21aad777c43b4601acb71
Lutrome <921778+Lutrome@users.noreply.github.com>
2020-06-01 21:02:42 -0700

    QueerBot v0.0.3
    - Add Bot Onbject (queerBotMain.py#L5)
    - Add queerBot.run function to bring it online (queerBotMain.py#L7)

--- src/queerBotImports.py
+++ src/queerBotImports.py
@@ -2,6 +2,8 @@
 from keys.discordKeys import *
 
 # Library Imports
-import discord # Discord.py Library (v1.4.0a)
+import discord as dpy # Discord.py Library (v1.4.0a)
+import discord.ext.commands as discordExtCmds # Discord.py Commands Extension
+import discord.ext.tasks as discordExtTasks # Discord.py Tasks Extension
 import boto3 # Boto3 API for Amazon Web Services (v1.13.11)
 import asyncio # Asynchronous Input/Output (Python Built-In)
--- src/queerBotMain.py
+++ src/queerBotMain.py
@@ -2,4 +2,6 @@
 from queerBotImports import *
 
 # Main Code
-queerBotClient = discord.Client() # Creates a Discord Client object
+queerBot = discordExtCmds.Bot(description="QueerBot") # Creates Discord Bot Object - Might replace this with Client Object for finer control.
+
+queerBot.run(discordBotToken) # Runs the queerBot and brings it online
